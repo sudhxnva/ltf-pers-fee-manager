edeea570b8f37e091b6728cd9297145a7aea2a7d
Sudhanva <sudhanva.m@pm.me>
2021-06-16 21:20:31 +0530

    Add winston logger

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const app = require('./app');
 const config = require('./config/config');
+const log = require('./config/logger');
 
 const server = app.listen(config.port, () => {
-  console.log(`Listening to port ${config.port}`);
+  log.info(`Listening to port ${config.port}`);
 });
 
 const exitHandler = () => {
   server.close(() => {
-    console.log('Server closed');
+    log.info('Server closed');
     process.exit(1);
   });
 };
 
 const unexpectedErrorHandler = (error) => {
-  console.log(error);
+  log.error(error);
   exitHandler();
 };
 
@@ -21,11 +22,11 @@ process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM received');
+  log.info('SIGTERM received');
   server.close();
 });
 
 process.on('SIGINT', () => {
-  console.log('SIGINT received');
+  log.info('SIGINT received');
   server.close();
 });
